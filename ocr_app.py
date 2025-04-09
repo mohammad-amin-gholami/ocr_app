957cb8e197430ae1efb07eca17aeb533e997b965
amin gholami <107848148+mohammad-amin-gholami@users.noreply.github.com>
2025-04-09 17:53:41 +0330

    Add files via upload

---
+++ ocr_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from PIL import Image
+import pytesseract
+from googletrans import Translator
+from gtts import gTTS
+import os
+import asyncio
+
+st.title("OCR استخراج")
+
+uploaded_file = st.file_uploader("یک تصویر آپلود کن", type=["jpg", "png", "jpeg"])
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='تصویر آپلود شده', use_container_width=True)
+
+
+    lang_choice = st.selectbox("زبان متن داخل تصویر:", ["فارسی", "انگلیسی"])
+    lang_code = 'fas' if lang_choice == "فارسی" else 'eng'
+
+    if st.button("استخراج متن"):
+        text = pytesseract.image_to_string(image, lang=lang_code)
+        st.subheader("متن استخراج‌شده:")
+        st.text_area("", text, height=200)
+
+        # translator = Translator()
+        # dest_lang = 'en' if lang_code == 'fas' else 'fa'
+        # translated = translator.translate(text, src=lang_code, dest=dest_lang)
+        # st.subheader("ترجمه:")
+        # translated = asyncio.run(translate_text(translated))
+        # st.text_area("", translated.text, height=200)
+
+
+        # tts_lang = 'fa' if dest_lang == 'fa' else 'en'
+        # tts = gTTS(translated.text, lang=tts_lang)
+        # tts.save("output.mp3")
+        # audio_file = open("output.mp3", "rb")
+        # st.audio(audio_file.read(), format="audio/mp3")
+
+
+        # os.remove("output.mp3")
