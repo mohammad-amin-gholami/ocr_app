2fb35ef08494d440816d51f3843eaa5f48d20502
amin gholami <107848148+mohammad-amin-gholami@users.noreply.github.com>
2025-04-09 18:13:18 +0330

    Update ocr_app.py

--- ocr_app.py
+++ ocr_app.py
@@ -4,7 +4,7 @@ import pytesseract
 from googletrans import Translator
 from gtts import gTTS
 import os
-import asyncio
+
 
 st.title("OCR استخراج")
 
@@ -13,15 +13,16 @@ if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image, caption='تصویر آپلود شده', use_container_width=True)
 
-
+# انتخاب زبان
     lang_choice = st.selectbox("زبان متن داخل تصویر:", ["فارسی", "انگلیسی"])
     lang_code = 'fas' if lang_choice == "فارسی" else 'eng'
-
+# استخراج متن با توجه به زبان انتخاب شده 
     if st.button("استخراج متن"):
         text = pytesseract.image_to_string(image, lang=lang_code)
         st.subheader("متن استخراج‌شده:")
         st.text_area("", text, height=200)
 
+# ترجمه متن به زبان های دیگر
         # translator = Translator()
         # dest_lang = 'en' if lang_code == 'fas' else 'fa'
         # translated = translator.translate(text, src=lang_code, dest=dest_lang)
@@ -29,7 +30,7 @@ if uploaded_file:
         # translated = asyncio.run(translate_text(translated))
         # st.text_area("", translated.text, height=200)
 
-
+# تبدیل به فایل صوتی 
         # tts_lang = 'fa' if dest_lang == 'fa' else 'en'
         # tts = gTTS(translated.text, lang=tts_lang)
         # tts.save("output.mp3")
